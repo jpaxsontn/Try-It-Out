950b9d0039d3baf72f9891ecfa8cfa0b14993041
jpaxsontn <jpaxsontn@yahoo.com>
2015-11-13 11:26:15 -0600

    Completed pg 79

--- Pg 79/Program.cs
+++ Pg 79/Program.cs
@@ -10,6 +10,27 @@ namespace Pg_79
     {
         static void Main(string[] args)
         {
+            for(var iCounter = 1; iCounter <= 100; iCounter++)
+            {
+                if(iCounter %3 == 0 && iCounter % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(iCounter %3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if(iCounter %5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(iCounter);
+                }
+            }
+
+            Console.Read();
         }
     }
 }
