2abfab9997f2fef1a75e8728c28a86091c8094fe
jpaxsontn <jpaxsontn@yahoo.com>
2015-11-13 11:01:33 -0600

    Initial Checkin

---
+++ Pg 78/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//This was a little tricky and took me a little over an hour to figure out.
+//Shame on me instead of solving with pencil and paper (Actually thinking about the problem) 
+//I tried several different and wrong combos first before I thought through the problem.
+
+namespace Pg_78
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintPyramid(11);
+            Console.ReadLine();
+        }
+
+        public static void PrintPyramid(int height)
+        {
+            int baseWidth = 0;
+            if(height == 1)
+            {
+                baseWidth = 1;
+            }
+            else
+            {
+                baseWidth = (height * 2);
+            }
+
+            for(var iHeight = 0; iHeight < height; iHeight++)
+            {
+                for(var iBaseWidth = 0; iBaseWidth < baseWidth; iBaseWidth++)
+                {
+                    if(iBaseWidth >= (height - iHeight))
+                    {
+                        if(iBaseWidth > (height + iHeight))
+                        {
+                            Console.Write(" ");
+                        }
+                        else
+                        {
+                            Console.Write("*");
+                        }
+                    }
+                    else if(baseWidth == height)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ Pg 79/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg_79
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
