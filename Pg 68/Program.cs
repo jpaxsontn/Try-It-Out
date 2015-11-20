90bde9b4439d883306243cd7594c17fea2815596
jpaxsontn <jpaxsontn@yahoo.com>
2015-11-19 22:00:41 -0600

    Added Pg 72 and Pg 68 Exercises

---
+++ Pg 68/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg_68
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            var numberOne = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            var numberTwo = Int32.Parse(Console.ReadLine());
+
+            var message = "";
+
+            if ((numberOne > 0 && numberTwo > 0) || (numberOne < 0 && numberTwo < 0))
+            {
+                message = "Result is positive";
+            }
+            else
+            {
+                message = "Result is negetive";
+            }
+
+            Console.WriteLine(message);
+            ShowExit();
+        }
+
+        private static void ShowExit()
+        {
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Pg 72/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg_72
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the calculator");
+            Console.WriteLine("Enter the first number");
+            var numberOne = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            var numberTwo = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter an operation +, -, *, /, %, ^");
+            var operation = Console.ReadLine();
+            var result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = numberOne + numberTwo;
+                    break;
+                case "-":
+                    result = numberOne - numberTwo;
+                    break;
+                case "*":
+                    result = numberOne * numberTwo;
+                    break;
+                case "/":
+                    result = numberOne / numberTwo;
+                    break;
+                case "%":
+                    result = numberOne % numberTwo;
+                    break;
+                case "^":
+                    result = Int32.Parse(Math.Pow(numberOne, numberTwo).ToString());
+                    break;
+            }
+
+            Console.WriteLine("Result of " + numberOne + " " + operation + " " + numberTwo + " = " + result);
+            ShowExit();
+        }
+
+        private static void ShowExit()
+        {
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
