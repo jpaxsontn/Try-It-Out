41de165457292c1501ae55e1f9993c739cc7f75c
jpaxsontn <jpaxsontn@yahoo.com>
2015-12-09 10:44:38 -0600

    Added Pg 83 and Pg 85

---
+++ Pg 83/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg_83
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] secondArray = new int[firstArray.Length];
+
+            for(var iIndex = 0; iIndex < firstArray.Length; iIndex++)
+            {
+                secondArray[iIndex] = firstArray[iIndex];
+            }
+
+            for (var iIndex = 0; iIndex < firstArray.Length; iIndex++)
+            {
+                Console.WriteLine("firstArray[" + iIndex + "] = " + firstArray[iIndex]);
+                Console.WriteLine("secondArray[" + iIndex + "] = " + secondArray[iIndex]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Pg 85/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg_85
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArray = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+            //int[] copyOfNumbersArray = new int[numbersArray.Length];
+
+            int total = 0;
+
+            foreach(var iNumber in numbersArray)
+            {
+                total += iNumber;
+                //copyOfNumbersArray[I don't have the index]
+            }
+
+            float average = (float)total / numbersArray.Length;
+
+            Console.WriteLine("Sum of numbers in array = " + total);
+            Console.WriteLine("Average of numbers in array = " + average);
+
+            Console.ReadKey();
+        }
+    }
+}
